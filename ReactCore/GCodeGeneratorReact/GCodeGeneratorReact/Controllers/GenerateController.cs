137fda3e355f0fe52af4e410891ecb7246afca0a
plarium\d.revenko <d.revenko@plarium.com>
2022-09-23 18:40:03 +0300

    Add presets.

--- ReactCore/GCodeGeneratorReact/GCodeGeneratorReact/Controllers/GenerateController.cs
+++ ReactCore/GCodeGeneratorReact/GCodeGeneratorReact/Controllers/GenerateController.cs
@@ -3,6 +3,7 @@ using ElectronNET.API.Entities;
 using GCodeGeneratorReact.Dtos;
 using GCodeGeneratorReact.Services;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 
 namespace GCodeGeneratorReact.Controllers
 {
@@ -24,7 +25,7 @@ namespace GCodeGeneratorReact.Controllers
 			{
 				Title = "Save G-code",
 				Filters = new FileFilter[]{
-				new FileFilter { Name = "G-codes", Extensions = new string[] {"gcode", "txt"} }
+					new FileFilter { Name = "G-codes", Extensions = new string[] {"gcode", "txt"} }
 				}
 			};
 
@@ -41,6 +42,62 @@ namespace GCodeGeneratorReact.Controllers
 			{
 				return 0;
 			}
+		}
+
+		[HttpPost]
+		[Route("preset")]
+		public async Task<int> SavePreset(GeneratorRequestDto dto)
+		{
+			var options = new SaveDialogOptions
+			{
+				Title = "Save G-code",
+				Filters = new FileFilter[]{
+					new FileFilter { Name = "G-codes", Extensions = new string[] {"gcode", "txt"} }
+				}
+			};
+			try
+			{
+				var mainWindow = Electron.WindowManager.BrowserWindows.First();
+				var result = await Electron.Dialog.ShowSaveDialogAsync(mainWindow, options);
+				if (result == null)
+					return 0;
+				using var fs = new StreamWriter(result, false);
+				await JsonSerializer.SerializeAsync(fs.BaseStream, dto);
+
+				return 1;
+			}
+			catch
+			{
+				return 0;
+			}
+		}
+
+		[HttpGet]
+		[Route("preset")]
+		public async Task<GeneratorRequestDto> OpenPreset()
+		{
+			var options = new OpenDialogOptions
+			{
+				Title = "Save preset",
+				Filters = new FileFilter[]{
+					new FileFilter { Name = "Preset", Extensions = new string[] {"gcode-preset"} }
+				},
+				Properties = new[] { OpenDialogProperty.openFile }
+			};
+
+			var mainWindow = Electron.WindowManager.BrowserWindows.First();
+			var result = (await Electron.Dialog.ShowOpenDialogAsync(mainWindow, options))?.FirstOrDefault();
+
+			if (string.IsNullOrWhiteSpace(result))
+				throw new Exception("no file");
+			if (!System.IO.File.Exists(result))
+				throw new Exception("no file");
+
+			using var fs = System.IO.File.OpenRead(result);
+			var preset = JsonSerializer.Deserialize<GeneratorRequestDto>(fs);
+			if (preset == null)
+				throw new Exception("wrong file");
+			return preset;
 		}
 	}
 }
